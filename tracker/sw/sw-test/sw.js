e58529729c9958c461280ed89d18c9a388550a78
dimi <dlee@mozilla.com>
2019-11-22 09:52:41 +0100

    add sw

---
+++ tracker/sw/sw-test/sw.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        './sw-test/',
+        './sw-test/index.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    try {
+      let url = "https://itisatracker.org/index.html";
+      const response = await fetch(url, { mode: 'cors' });
+    } catch(e) {
+    }
+  })());
+});
+
+self.addEventListener('fetch', event => {
+});
+
+self.addEventListener('message', event => {
+});
